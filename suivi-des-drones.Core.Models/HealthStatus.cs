ea24c761a033d7fab1106a79e798400756fe5414
wolf <dairhamza@gmail.com>
2024-07-11 15:49:21 +0100

    Modifier la class HealthStatus : Mettre en commentaire la class et la remplacer par la Methode 3 , un Record avec des attribus immutables en utilisant l'option Init dans les attribut STATIC

--- suivi-des-drones.Core.Models/HealthStatus.cs
+++ suivi-des-drones.Core.Models/HealthStatus.cs
@@ -9,7 +9,7 @@ namespace suivi_des_drones.Core.Models
     /// <summary>
     /// Les status possibles d'une drone
     /// </summary>
-    public class HealthStatus
+    public record HealthStatus
     {
         #region DroneStatus
         public static HealthStatus Ok = new HealthStatus { Id = 0, Label = "Ok" };
@@ -18,10 +18,23 @@ namespace suivi_des_drones.Core.Models
         #endregion
 
         #region Properties
-        public int Id { get; set; } = 0;
-        public string Label { get; set; } = default!;
+        public int Id { get; init; }
+        public string Label { get; init; } = default!;
         #endregion
     }
+    //public class HealthStatus
+    //{
+    //    #region DroneStatus
+    //    public static HealthStatus Ok = new HealthStatus { Id = 0, Label = "Ok" };
+    //    public static HealthStatus Broken = new HealthStatus { Id = -1, Label = "Cassé" };
+    //    public static HealthStatus Repair = new HealthStatus { Id = -2, Label = "En réparation" };
+    //    #endregion
+
+    //    #region Properties
+    //    public int Id { get; set; } = 0;
+    //    public string Label { get; set; } = default!;
+    //    #endregion
+    //}
     //public enum HealthStatus
     //{
     //    Ok = 0,
