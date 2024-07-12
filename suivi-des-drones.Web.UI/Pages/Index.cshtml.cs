a1bfd68c30837960e8e06ed8c42e0d57151b6f29
wolf <dairhamza@gmail.com>
2024-07-12 17:33:22 +0100

    Dans IndexModel : Modifier le nom de la liste des drones : remplacer Lista par ListOfDrones - Ajouter une propriété de type List string : ListOfStatus - Ajouter la Methode de type qui retourne une list des tring SetListOfStatus qui recupère la liste des status a partir de l'attribut Label de la class HealthStatus - Ajouter dans Index.cshtml une lite déroulante des status - Chargement de la liste deroulante fait avec succès

--- suivi-des-drones.Web.UI/Pages/Index.cshtml.cs
+++ suivi-des-drones.Web.UI/Pages/Index.cshtml.cs
@@ -9,17 +9,20 @@ namespace suivi_des_drones.Web.UI.Pages
     {
 
         #region Properties
-        public List<Drone> Lista = new List<Drone>();
+        public List<Drone> ListOfDrones = new List<Drone>();
+        public List<string> ListOfStatus = new List<string>();
         IDroneRepository repository;
+        //HealthStatus healthStatus;
         #endregion
 
 
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger, IDroneRepository repository)
+        public IndexModel(ILogger<IndexModel> logger, IDroneRepository repository /*, HealthStatus healthStatus*/)
         {
             _logger = logger;
             this.repository = repository;
+            //this.healthStatus = healthStatus;
         }
 
         //public void OnGet()
@@ -29,13 +32,23 @@ namespace suivi_des_drones.Web.UI.Pages
 
         public IActionResult OnGet()
         {
-            setList();
-            return Page();
+            SetDroneList();
+            SetStatusList();
+            return this.Page();
         }
 
-        public List<Drone> setList()
+        public List<Drone> SetDroneList()
         {
-            return Lista = repository.GetAll();
+            ListOfDrones = repository.GetAll();
+            return ListOfDrones;
+        }
+
+        public List<string> SetStatusList()
+        {
+            ListOfStatus.Add(HealthStatus.Ok.Label);
+            ListOfStatus.Add(HealthStatus.Broken.Label);
+            ListOfStatus.Add(HealthStatus.Repair.Label);
+            return ListOfStatus;
         }
 
 
